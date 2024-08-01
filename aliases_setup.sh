d23c1d24636e49868d3cd78813ecf38403046335
David Huckfield <djhuckfield@hotmail.co.uk>
2024-08-01 16:49:15 +0100

    Saved file with alias setup code

---
+++ aliases_setup.sh
@@ -0,0 +1,6 @@
+#sets up "l" as an alias to log last 10 commits, one line only showing commit code and subject, and include graph lines of branches
+git config --global alias.l "log --oneline --graph --n10"
+#sets up alias of "s" for status
+git config --global alias.s "status"
+#sets up alias of "co" for checkout
+git config --global alias.co "checkout"
